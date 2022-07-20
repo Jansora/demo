88178e2827fb668e88292ac40a20832748894012
Jansora <zhangyue1936@gmail.com>
2022-07-20 14:21:54 +0800

    init frontend demo

---
+++ frontend/react-rich-editor/src/rich-editor/loading/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Spin from 'antd/es/spin';
+import 'antd/es/spin/style';
+
+
+const Loading = ({text, loading, children}) => {
+	return (
+		<Spin className="loading" tip={text} spinning={loading}>
+			{children}
+		</Spin>
+	);
+};
+
+export default Loading;
